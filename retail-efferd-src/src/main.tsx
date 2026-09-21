813a01feb21bf0099e0f294835abd776063a5210
lsaucen <lauranunezsalce@gmail.com>
2026-09-21 15:00:55 -0400

    Default Customer Retention dashboard to dark mode

--- retail-efferd-src/src/main.tsx
+++ retail-efferd-src/src/main.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="customer-retention-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="customer-retention-theme">
       <App />
     </ThemeProvider>
   </StrictMode>
