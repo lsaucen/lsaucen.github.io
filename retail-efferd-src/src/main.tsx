98890bec27e24f71f6460fb7d633dc1ea6fc76c8
lsaucen <lauranunezsalce@gmail.com>
2026-09-21 15:05:35 -0400

    Set Financial Intelligence dark default

--- retail-efferd-src/src/main.tsx
+++ retail-efferd-src/src/main.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="customer-retention-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="financial-intelligence-theme">
       <App />
     </ThemeProvider>
   </StrictMode>
